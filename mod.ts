389d3beee23f7e7eea9ec7f4025f003a735057f8
wesauis <54046750+wesauis@users.noreply.github.com>
2021-01-16 10:37:17 -0300

    more cli arguments, free and discounted providers
    - `--free` & `--discounted` arguments
    - separate free and discounted providers

--- mod.ts
+++ mod.ts
@@ -1,24 +1,28 @@
 import { disableLogger } from "./logging/logger.ts";
 import GoG from "./providers/GoG.ts";
 import GameOffer from "./types/GameOffer.d.ts";
+import { GameOfferProvider } from "./types/GameOfferProvider.d.ts";
 
 export const providers = {
-  "gog": new GoG(),
+  free: {
+    "gog": new GoG("free"),
+  },
+  discounted: {
+    "gog": new GoG("discounted"),
+  },
 };
 
-export type RegisteredProviders = keyof typeof providers;
+export type RegisteredProviders =
+  & keyof typeof providers.free
+  & keyof typeof providers.discounted;
 
-async function queryOffers(): Promise<GameOffer[]> {
+async function queryOffers(
+  providers: GameOfferProvider[],
+): Promise<GameOffer[]> {
   return await Promise.all(
-    Object.values(providers).map((provider) => provider.query()),
+    providers.map((provider) => provider.query()),
   ) // join the results
-    .then((offers) => offers.flat())
-    // order by discount, descending
-    .then((offers) =>
-      offers.sort((a, b) =>
-        (b.price?.discount || 100) - (a.price?.discount || 100)
-      )
-    );
+    .then((offers) => offers.flat());
 }
 
 if (import.meta.main) {
@@ -28,9 +32,13 @@ if (import.meta.main) {
     console.log(
       `Usage: game-db [OPTIONS]
 
+query games from all providers and output the json to the stdout
+
 Options: 
     --no-logging    disable logger
     --raw           dont prettify json
+    --free          use free games providers
+    --discounted    use free games providers
     --help          show this help message`,
     );
 
@@ -40,5 +48,19 @@ Options:
   if (Deno.args.includes("--no-logging")) disableLogger();
   if (Deno.args.includes("--raw")) spaces = undefined;
 
-  console.log(JSON.stringify(await queryOffers(), undefined, spaces));
+  const providers_: GameOfferProvider[] = [];
+  if (Deno.args.includes("--free")) {
+    providers_.push(...Object.values(providers.free));
+  }
+  if (Deno.args.includes("--discounted")) {
+    providers_.push(...Object.values(providers.discounted));
+  }
+  if (providers_.length === 0) {
+    providers_.push(
+      ...Object.values(providers.free),
+      ...Object.values(providers.discounted),
+    );
+  }
+
+  console.log(JSON.stringify(await queryOffers(providers_), undefined, spaces));
 }
