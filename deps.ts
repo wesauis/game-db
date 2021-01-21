208022d67b7cdb8d2c16b940a1c7fdb845ee30b1
wesauis <54046750+wesauis@users.noreply.github.com>
2021-01-21 20:22:44 -0300

    use specific versions of deps, closed #5

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { cyan, red, yellow } from "https://deno.land/std@0.83.0/fmt/colors.ts";
-export { getLogger, Logger } from "https://deno.land/std@0.83.0/log/mod.ts";
-export { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
-export type { Element } from "https://deno.land/x/deno_dom/src/dom/element.ts";
-export type { NodeList } from "https://deno.land/x/deno_dom/src/dom/node-list.ts";
-export type { HTMLDocument } from "https://deno.land/x/deno_dom/src/dom/document.ts";
+export { cyan, red, yellow } from "https://deno.land/std@0.84.0/fmt/colors.ts";
+export { getLogger, Logger } from "https://deno.land/std@0.84.0/log/mod.ts";
+export { DOMParser } from "https://deno.land/x/deno_dom@v0.1.3-alpha2/deno-dom-wasm.ts";
+export type { HTMLDocument } from "https://deno.land/x/deno_dom@v0.1.3-alpha2/src/dom/document.ts";
+export type { Element } from "https://deno.land/x/deno_dom@v0.1.3-alpha2/src/dom/element.ts";
+export type { NodeList } from "https://deno.land/x/deno_dom@v0.1.3-alpha2/src/dom/node-list.ts";
