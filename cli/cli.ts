4065324ddcf50e0ce9319646dc0375a94e046cab
wesauis <54046750+wesauis@users.noreply.github.com>
2021-01-23 21:47:20 -0300

    cli/feat filters, json and updated help
    -- update `--help`
    -- json mode with `--json`
    -- category and provider filter with `--categories` and `--providers`

--- cli/cli.ts
+++ cli/cli.ts
@@ -1,13 +1,13 @@
 if (!import.meta.main) throw new Error("cli only");
 
-import { parse as parseArgs } from "https://deno.land/std@0.84.0/flags/mod.ts";
-import { queryOffers } from "../lib/lib.ts";
+import {
+  Args,
+  parse as parseArgs,
+} from "https://deno.land/std@0.84.0/flags/mod.ts";
+import { listCategories, listNames, queryOffers } from "../lib/lib.ts";
+import logger, { Logger } from "../log/logger.ts";
 
-const args = parseArgs(Deno.args, {
-  boolean: ["help", "json"],
-});
-
-if (args["help"]) {
+function showHelpAndExit() {
   console.log(
     `Usage: game-db [OPTIONS]
 
@@ -15,7 +15,14 @@ query games from all providers and output the json to the stdout
 
 Options: 
   --help          show this help message
-  
+  --json          json mode: prints raw json to stdout and disables logger
+  --categories    categories to use separated by comma
+                  avaliable categories: ${[...listCategories()].join(", ")}
+                  example: \`--categories catg1,catg2\`
+  --providers     provider names to use separated by comma
+                  avaliable providers: ${[...listNames()].join(", ")}
+                  example: \`--providers name1,name2\`
+
 Env:
   NO_COLOR        disable colors`,
   );
@@ -23,10 +30,30 @@ Env:
   Deno.exit(0);
 }
 
-const games = await queryOffers();
+const args = parseArgs(Deno.args, {
+  boolean: ["help", "json"],
+  string: ["categories", "providers"],
+  unknown(arg) {
+    logger.warn("unknown argument", arg, "\n");
 
-if (args["json"]) {
-  console.log(JSON.stringify(games));
-} else {
-  throw new Error("unimplemented yet..., for now you need to use --json");
+    showHelpAndExit();
+  },
+}) as Args & {
+  help: boolean;
+  json: boolean;
+  categories?: string;
+  providers?: string;
+};
+
+if (args.help) showHelpAndExit();
+if (args.json) Logger.ENABLED = false;
+
+const categories = args.categories?.split(",");
+const providers = args.providers?.split(",");
+
+const games = await queryOffers(categories, providers);
+
+if (args.json) console.log(JSON.stringify(games));
+else {
+  console.log(games);
 }
