fe933cf5ac2d03cb298b58a5982aafd93cc32a72
wesauis <54046750+wesauis@users.noreply.github.com>
2021-02-06 19:15:57 -0300

    cli: use the searchers

--- cli/deps.ts
+++ cli/deps.ts
@@ -1,6 +1,5 @@
-export { parse as parseArgs } from "https://deno.land/std@0.84.0/flags/mod.ts";
-export type { Args } from "https://deno.land/std@0.84.0/flags/mod.ts";
-export * as colors from "https://deno.land/std@0.84.0/fmt/colors.ts";
-export { default as appPaths } from "https://github.com/wesauis/deno-app-paths/raw/0.1.1/mod.ts";
-export * from "../lib/lib.ts";
+export * as colors from "https://deno.land/std@0.85.0/fmt/colors.ts";
 export { existsSync } from "https://deno.land/std@0.85.0/fs/exists.ts";
+export { default as appPaths } from "https://github.com/wesauis/deno-app-paths/raw/0.1.1/mod.ts";
+export { parseArgs } from "https://github.com/wesauis/deno-typed-parse-args/raw/0.1.0/mod.ts";
+export * from "../lib/lib.ts";
---
+++ cli/search-offers.ts
@@ -0,0 +1,18 @@
+import type { Offer, OfferSearcher } from "./deps.ts";
+
+export async function searchOffers(
+  searchers: Record<string, OfferSearcher>,
+): Promise<Record<string, Offer[]>> {
+  const querys = Object.entries(searchers)
+    .map(([id, searcher]) => {
+      return { id, search: searcher() };
+    });
+
+  const offers: Record<string, Offer[]> = {};
+
+  for (const { id, search } of querys) {
+    offers[id] = await search;
+  }
+
+  return offers;
+}
