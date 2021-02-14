1da8ab559bbc49d751da684a3e5434267609eca8
wesauis <54046750+wesauis@users.noreply.github.com>
2021-02-14 19:29:50 -0300

    fix free and discounted

--- cli/utils/categories.ts
+++ cli/utils/categories.ts
@@ -1,50 +1,58 @@
 import { Offer } from "../../lib/lib.ts";
 import { map } from "../../lib/utils/map.ts";
+import { SearchResults } from "../search-offers.ts";
 import { Args } from "./args.ts";
 
 export type OfferAndCategory = Offer & {
   category?: "free" | "discounted" | "ending" | "best-offer";
 };
 
+export type CategorizedResults<T extends string | number | symbol = string> =
+  Record<T, OfferAndCategory[]>;
+
 export function categorize(
-  results: Record<string, Offer[]>,
+  results: SearchResults,
   options: {
     endingms: Args["endingms"];
     bestOffers: Args["bestOffers"];
     free: Args["free"];
     discounted: Args["discounted"];
   },
-) {
-  const categorized: Record<keyof typeof results, OfferAndCategory[]> = {};
+): CategorizedResults<keyof typeof results> {
+  const categorized: CategorizedResults<keyof typeof results> = {};
 
   const now = Date.now();
   for (const id in results) {
     categorized[id] = map(results[id], (offer: OfferAndCategory) => {
-      const isAlwaysFree = offer.price !== 0;
-      if (
-        options.endingms &&
-        isAlwaysFree && offer.discount?.endDate &&
-        offer.discount.endDate + options.endingms >= now
-      ) {
-        offer.category = "ending";
-      } else if (options.bestOffers && isAlwaysFree) {
-        const [percentage, amount] = options.bestOffers;
+      if (offer.price !== 0) {
+        if (
+          options.endingms &&
+          offer.discount?.endDate &&
+          offer.discount.endDate + options.endingms >= now
+        ) {
+          offer.category = "ending";
+        } else if (options.bestOffers) {
+          const [percentage, amount] = options.bestOffers;
 
-        if (percentage != null) {
-          const offerPercentage = offer.discount!.discountPercentage;
-          if (offerPercentage > percentage) {
-            offer.category = "best-offer";
-          }
-        } else if (amount != null) {
-          const offerSavedAmount = offer.price - offer.discount!.discountPrice;
-          if (offerSavedAmount > amount) {
-            offer.category = "best-offer";
+          if (percentage != null) {
+            const offerPercentage = offer.discount!.discountPercentage;
+            if (offerPercentage >= percentage) {
+              offer.category = "best-offer";
+            }
+          } else if (amount != null) {
+            const offerSavedAmount = offer.price -
+              offer.discount!.discountPrice;
+            if (offerSavedAmount >= amount) {
+              offer.category = "best-offer";
+            }
           }
         }
-      } else if (options.free && isAlwaysFree) {
+
+        if (!offer.category && options.discounted) {
+          offer.category = "discounted";
+        }
+      } else if (options.free) {
         offer.category = "free";
-      } else if (options.discounted) {
-        offer.category = "discounted";
       }
 
       return offer.category ? offer : undefined;
